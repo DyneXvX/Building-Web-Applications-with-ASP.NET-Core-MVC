9ccefb261f3553b318bfe37e3e3714c35f5cde3c
Justin Thoms <DyneXvX@outlook.com>
2020-06-14 15:39:06 -0400

    Built Mock Databases and set them up in the Startup ConfigureServices

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,10 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPieRepository, MockPieRepository>();// allows us to use our made up hardcoded database.
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            //services.AddTransient(); Create a new one every time you ask for one.
+            //services.AddSingleton(); Create a single instance for the entire app
             services.AddControllersWithViews();//MVC setup
         }
 
